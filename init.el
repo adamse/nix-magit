21c292eff86cd493019fad1ae3a6b78399838f65
Adam Sandberg Eriksson <adam@sandbergericsson.se>
2020-06-16 16:21:20 +0100

    day theme

--- init.el
+++ init.el
@@ -5,7 +5,7 @@
 
 ;; Set up theme
 (require 'color-theme-sanityinc-tomorrow)
-(load-theme 'sanityinc-tomorrow-bright t)
+(load-theme 'sanityinc-tomorrow-day t)
 
 ;; Start magit-status
 (setq magit-display-buffer-function #'magit-display-buffer-same-window-except-diff-v1)
